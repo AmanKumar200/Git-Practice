32073bc0e55498c9e4fd8a9a7409302921ce8c57
AmanKumar200 <aman.ak.ak63@gmail.com>
2026-06-24 17:24:40 +0530

    Remove duplicate line

--- aman.py
+++ aman.py
@@ -4,6 +4,5 @@ print("I always try to learn new things and improve my skills")
 
 print("I am currently learning Data Analysis and Machine Learning")
 print("I am also working on some projects to enhance my knowledge and experience")
-print("I have a strong passion for data analysis and programming, and I am constantly seeking opportunities to enhance my skills and contribute to meaningful projects.")
 print("I am proficient in Python, SQL, and data visualization tools, and I enjoy working with data to uncover insights and drive informed decision-making.")
 print("I have a strong passion for data analysis and programming, and I am constantly seeking opportunities to enhance my skills and contribute to meaningful projects.")
