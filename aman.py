9aedbe7af2eddc00096c552dc8704ee8b23055cd
AmanKumar200 <aman.ak.ak63@gmail.com>
2026-06-24 16:16:55 +0530

    new change

--- aman.py
+++ aman.py
@@ -2,3 +2,4 @@ print("Hello, my name is Aman Kumar")
 print("I am a Aspiring Data Analyst and a Python Developer")
 print("I always try to learn new things and improve my skills")
 
+print("I am currently learning Data Analysis and Machine Learning")
