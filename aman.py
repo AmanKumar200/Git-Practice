e592dac39d5e0be9f232cb6294032850493e6fc5
AmanKumar200 <aman.ak.ak63@gmail.com>
2026-06-24 17:10:33 +0530

    new changes

--- aman.py
+++ aman.py
@@ -1,3 +1,4 @@
 print("Hello, my name is Aman Kumar")
 print("I am a Aspiring Data Analyst and a Python Developer")
 print("I have a strong passion for data analysis and programming, and I am constantly seeking opportunities to enhance my skills and contribute to meaningful projects.")
+print("I am proficient in Python, SQL, and data visualization tools, and I enjoy working with data to uncover insights and drive informed decision-making.")
