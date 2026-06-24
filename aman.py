70733bbed967a77796b5e26145f138095c961dd4
AmanKumar200 <aman.ak.ak63@gmail.com>
2026-06-24 16:28:07 +0530

    new change

--- aman.py
+++ aman.py
@@ -3,3 +3,4 @@ print("I am a Aspiring Data Analyst and a Python Developer")
 print("I always try to learn new things and improve my skills")
 
 print("I am currently learning Data Analysis and Machine Learning")
+print("I am also working on some projects to enhance my knowledge and experience")
